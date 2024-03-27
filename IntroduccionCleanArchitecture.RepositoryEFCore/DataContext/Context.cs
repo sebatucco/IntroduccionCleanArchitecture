2f508c4ea10f65e3aeff0431a69c36be552c186a
sebatucco <c.sebastian.ojeda@gmail.com>
2024-03-27 11:14:07 -0300

    subo proyecto

---
+++ IntroduccionCleanArchitecture.RepositoryEFCore/DataContext/Context.cs
@@ -0,0 +1,18 @@
+﻿using IntroduccionCleanArchitecture.Entities.POCOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitecture.Repository.EFCore.DataContext
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }    
+    }
+}
---
+++ IntroduccionCleanArchitecture.RepositoryEFCore/DataContext/ContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitecture.Repository.EFCore.DataContext
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+           var OptionBuilder = new DbContextOptionsBuilder<Context>();
+            OptionBuilder.UseSqlServer("Data Source = DESKTOP-0KJ2PDV; Initial Catalog=IntroduccionCleanArchitecture; Integrated Security=true; TrustServerCertificate=True");
+            return new Context(OptionBuilder.Options);
+        }
+    }
+}
