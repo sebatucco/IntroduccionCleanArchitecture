d7df6e594c7dfc0fa93eb0394794b27c1664891b
sebatucco <c.sebastian.ojeda@gmail.com>
2024-03-28 12:57:28 -0300

    creacion de interfaces e interactor

---
+++ IntroduccionCleanArchitecture.UseCases/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using IntroduccionCleanArchitecture.UseCases.CreateProduct;
+using IntroduccionCleanArchitecture.UseCases.GetAllProducts;
+using IntroduccionCleanArchitecture.UseCasesPorts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitecture.UseCases
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddUseCasesServices(this IServiceCollection services)
+        {
+            services.AddTransient<ICreateProductInputPort, CreateProductInteractor>();
+            services.AddTransient<IGetAllProductInputPort, GetAllProductsInteractor>();
+            return services;
+        }
+    }
+}
---
+++ IntroduccionCleanArchitecture.UseCases/GetAllProducts/GetAllProductsInteractor.cs
@@ -0,0 +1,26 @@
+﻿using IntroduccionCleanArchitecture.DTOs;
+using IntroduccionCleanArchitecture.Entities.Interfaces;
+using IntroduccionCleanArchitecture.UseCasesPorts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitecture.UseCases.GetAllProducts
+{
+    public class GetAllProductsInteractor : IGetAllProductInputPort
+    {
+        readonly IProductRepository _productRepository;
+        readonly IGetAllProductOutputPort _getAllProductOutputPort;
+
+        public GetAllProductsInteractor(IProductRepository productRepository, IGetAllProductOutputPort getAllProductOutputPort) =>
+        (_productRepository, _getAllProductOutputPort) = (productRepository, getAllProductOutputPort);
+        public Task Handle()
+        {
+            var products = _productRepository.GetAll().Select(p => new ProductDTO { Id = p.Id, Name = p.Name });
+            _getAllProductOutputPort.Handle(products);
+            return Task.CompletedTask;
+        }
+    }
+}
